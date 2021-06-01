9f7899b1f93d9fee5dfe95cbbb2e26cd88520fe2
Datakda <gagan9393@gmail.com>
2021-06-01 22:15:58 +0300

    Добавьте файлы проекта.

---
+++ Controllers/UpdateController.cs
@@ -0,0 +1,38 @@
+﻿using ClientListener.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClientListener.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UpdateController : ControllerBase
+    {
+        IHubContext<ChatHub> hubContext;
+        public UpdateController(IHubContext<ChatHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task UpdateAsync(RandomDouble json) 
+        {
+
+          
+            var rnd = json.result.ToString();
+
+            await hubContext.Clients.All.SendAsync("Send",rnd);
+
+
+
+
+        }
+
+    }
+}
